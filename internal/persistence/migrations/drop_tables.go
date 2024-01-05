b4b77814acb0064671effaff71adf74636bb0e22
Eduardo Amaral <eduardo.lopes.amaral@outlook.com>
2024-01-05 13:46:27 -0300

    🔄 new migration method

---
+++ internal/persistence/migrations/drop_tables.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"Orgzz-Tasks/config"
+	"Orgzz-Tasks/internal/models"
+)
+
+func main() {
+	db := config.OpenDbConnection()
+	// Migrate the schema
+	db.Migrator().DropTable(&models.Task{})
+}
